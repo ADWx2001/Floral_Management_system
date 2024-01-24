10edfdcdc798604baa5a371c728f908734bb77ad
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-01-24 13:30:41 +0530

    pages created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,22 @@
-
+import { BrowserRouter , Route , Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import DashBoard from "./pages/DashBoard"
+import Events from "./pages/Events"
 
 export default function App() {
   return (
-    <div className="text-3xl">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="/events" element={<Events/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
