ce0c8c19f43a70b557c5d6880d9a944e65ea943a
asanka119 <asankadilshanme@gmail.com>
2024-01-26 17:04:37 +0530

    created responsive product Tile

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,6 +4,11 @@ import Image4 from "../assests/img/home4.jpg"
 import ProductTile from "../components/ProductTile";
 import Slider from "../components/Slider";
 
+// Test Sample Product API
+  import product from "../assests/product/Product";
+
+console.log(product);
+
 export default function Home() {
   return (
     <div className="mx-auto justify-center">
@@ -11,11 +16,18 @@ export default function Home() {
       <div className="text-center pt-3">
         <h1 className="text-3xl text-rose-700 uppercase font-semibold">Flower Items</h1>
       </div>
-      <div className="flex gap-14 max-w-screen-xl mx-auto mt-16 mb-10">
+      <div className="flex flex-wrap gap-10 max-w-screen-xl mx-auto mt-16 mb-10 justify-center">
         {/* Image card */}
-        <ProductTile img={Image2} productName={"Rose Bucket"} deliveryTime={"Delivery in 2 days"} availability={"In Stock"} price={"$45"}/>
-        <ProductTile img={Image3} productName={"Rose Bucket"} deliveryTime={"Delivery in 2 days"} availability={"Out of Stock"} price={"$24"}/>
-        <ProductTile img={Image4} productName={"Rose Bucket"} deliveryTime={"Delivery in 2 days"} availability={"In Stock"} price={"$88"}/>
+        {product.map((singleProduct) => (
+          <ProductTile
+            key={singleProduct._id} // Make sure to use a unique key for each product
+            img={singleProduct.images} // Assuming the first image in the array is the main image
+            productName={singleProduct.name}
+            deliveryTime="Delivery in 2 days"
+            availability={singleProduct.availability ? "In Stock" : "Out of Stock"}
+            price={`$${singleProduct.price}`}
+          />
+      ))}
       </div>
     </div>
   )
