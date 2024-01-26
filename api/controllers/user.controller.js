762b07b66fc66a4471da3c6c47be7bce79f47551
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-01-26 20:56:42 +0530

    delete api finished

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -57,3 +57,15 @@ export const updateUser = async (req,res,next) => {
     }
 
 }
+export const deleteUser = async(req,res,next)=>{
+  if (req.user.id !== req.params.id) {
+      return next(errorHandler(401,'You can delete only your Account'));
+  }
+
+  try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been Deleted...")
+  } catch (error) {
+      next(error)
+  }
+}
