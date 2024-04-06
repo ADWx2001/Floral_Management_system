7d08476aa6591aa25d0fda2e0f4745c6ad4ae147
user <user@usernoMacBook-Pro.local>
2024-04-06 15:18:55 +0530

    Delivery management Crud Finished

---
+++ Frontend/src/components/DashProduct.jsx
@@ -0,0 +1,193 @@
+import { Button, Modal, Table } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiGift, HiOutlineExclamationCircle} from "react-icons/hi";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+export default function DashProduct() {
+    const { currentUser } = useSelector((state) => state.user);
+    const [userProduct, setUserProduct] = useState([]);
+    const [showMore, setShowMore] = useState(true);
+    const [showModel , setShowModel] = useState(false);
+    const [productIdToDelete, setProductIdToDelete] = useState('');
+    const [totalProducts, setTotalProducts] = useState(0);
+    const [lastMonthProducts, setlastMonthProducts] = useState(0);
+
+    useEffect(() => {
+      const fetchPosts = async () => {
+        try {
+          const res = await fetch(`/api/products/getproducts`);
+          const data = await res.json();
+          if (res.ok) {
+            setUserProduct(data.products);
+            setTotalProducts(data.totalProducts)
+            setlastMonthProducts(data.lastMonthProducts)
+            if (data.products.length < 9) {
+              setShowMore(false);
+            }
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+    
+      fetchPosts(); 
+    }, []);
+    
+    
+
+      const handleShowMore = async () => {
+        const startIndex = userProduct.length;
+        try {
+          const res = await fetch(
+            `/api/products/getproducts?userId=${currentUser._id}&startIndex=${startIndex}`
+          );
+          const data = await res.json();
+          if (res.ok) {
+            setUserProduct((prev) => [...prev, ...data.products]);
+            if (data.products.length < 9) {
+              setShowMore(false);
+            }
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+      const handleDeleteProduct = async () => {
+        setShowModel(false);
+        try {
+          const res = await fetch(
+            `/api/products/deleteproduct/${productIdToDelete}/${currentUser._id}`,
+            {
+              method: 'DELETE',
+            }
+          );
+          const data = await res.json();
+          if (!res.ok) {
+            console.log(data.message); 
+          } else {
+            setUserProduct((prev) =>
+              prev.filter((products) => products._id !== productIdToDelete)
+            );
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+      
+
+  return (
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      <div className='flex-wrap flex gap-4 justify-center'>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>Total Products</h3>
+              <p className='text-2xl'>{totalProducts}</p>
+            </div>
+            <HiGift className='bg-red-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+        </div>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>
+                Last Month Products
+              </h3>
+              <p className='text-2xl'>{lastMonthProducts}</p>
+            </div>
+            <HiGift className='bg-lime-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+        </div>
+        
+      </div>
+      {currentUser.isAdmin && userProduct.length>0?
+      (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Product Image</Table.HeadCell>
+              <Table.HeadCell>Product Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Supplier</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+              {userProduct.map((products)=>(
+                // eslint-disable-next-line react/jsx-key
+                <Table.Body  className='divide-y'  key={products._id}>
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell>{new Date(products.updatedAt).toLocaleDateString()}</Table.Cell>
+                    <Table.Cell>
+                    <Link to='`/product/${products._id}`'>
+                       <img
+                        src={products.image}
+                        alt={products.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                       />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link className='font-medium text-gray-900 dark:text-white'to={`/product/${products.slug}`}>
+                      {products.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{products.category}</Table.Cell>
+                  <Table.Cell>{products.supplier}</Table.Cell>
+                  <Table.Cell>
+                    <span className='font-medium text-red-500 hover:underline cursor-pointer'
+                        onClick={() => {
+                          setShowModel(true);
+                          setProductIdToDelete(products._id);
+                        }} 
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link className='text-teal-500 hover:underline'to={`/update-product/${products._id}`}>
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                  </Table.Row>
+                 
+                </Table.Body>
+              ))}
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
+        </>
+
+      ):(
+        <p>You have no products to show</p>
+      )}
+        <Modal show={showModel} onClose={()=>setShowModel(false)} popup size='md'>
+          <Modal.Header/>
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto"/>
+              <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-200">Are you sure you want to Delete this Product</h3>
+            </div>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteProduct}>
+                Yes, I am sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModel(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </Modal.Body>
+      </Modal>
+    </div>
+  )
+}
+
---
+++ api/models/order.model.js
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose'
+
+const orderSchema = new mongoose.Schema({
+    orderId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userId: {
+        type:String,
+        required : true,
+    },
+    productsId:{
+        type:String,
+        required: true,
+    },
+    first_name:{
+        type:String,
+        required:true,
+    },
+    last_name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,  
+        lowercase:true, 
+    },
+    phone:{
+        type:Number,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    state:{
+        type:String,
+        required:true,
+    },
+    zip:{
+        type:Number,
+        required:true,
+    },
+    subtotal:{
+        type: Number,
+        default:0.00,
+    },
+    deliveryfee:{
+        type: Number,
+        default:300,
+    },
+    totalcost:{
+       type: Number,
+       required:true,
+    },
+    // orderstatus:{
+    //     type: String,
+    //     enum:['pending','processing','completed', 'cancelled'],
+    // },
+    // paymentstatus:{
+    //     type: String,
+    // },
+    }, {timestamps: true}
+
+);
+
+const Order = mongoose.model('Order',orderSchema);
+
+export default Order;
