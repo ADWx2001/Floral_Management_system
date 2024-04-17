beef6b085be4bd8817936bb1cfe0225043d2e31f
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-04-17 10:59:05 +0530

    My order page and finction finished related to customer management

--- Frontend/src/components/DashMyOrders.jsx
+++ Frontend/src/components/DashMyOrders.jsx
@@ -26,51 +26,52 @@ export default function DashMyOrders() {
   }, [currentUser]);
 
   return (
-    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
-      <h2 className='my-7 text-center font-semibold text-3xl'>My Orders</h2>
+    <div className='table-auto overflow-x-scroll mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+       <h2 className='my-7 text-center font-semibold text-3xl'>My Orders</h2>
 
-      
-      {orders.length > 0 ? (
-        <>
-            <Table hoverable className="shadow-md">
+    {orders.length > 0 ? (
+      <>
+        <Table hoverable className='shadow-md'>
           <Table.Head>
-            <Table.Row>
-              <Table.HeadCell>Order ID</Table.HeadCell>
-              <Table.HeadCell>Product Title</Table.HeadCell>
-              <Table.HeadCell>Quantity</Table.HeadCell>
-              <Table.HeadCell>Sub Total</Table.HeadCell>
-              <Table.HeadCell>Delivery Fee</Table.HeadCell>
-              <Table.HeadCell>Total Cost</Table.HeadCell>
-            </Table.Row>
+            <Table.HeadCell>Order ID</Table.HeadCell>
+            <Table.HeadCell>Product Title</Table.HeadCell>
+            <Table.HeadCell>Quantity</Table.HeadCell>
+            <Table.HeadCell>Sub Total</Table.HeadCell>
+            <Table.HeadCell>Delivery Fee</Table.HeadCell>
+            <Table.HeadCell>Total Cost</Table.HeadCell>
           </Table.Head>
-          
-         {orders.map(order => (
-             <Table.Body  className='' key={order.orderId}>
-              <Table.Row  className='bg-white dark:border-gray-700 dark:bg-gray-800' >
-                <Table.Cell>{order.orderId}</Table.Cell>
+          {orders.map((order) => (
+            <Table.Body className='divide-y' key={order.orderId}>
+              <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                 <Table.Cell>
-                  {order.productsId.map(product => (
-                    <p key={product._id} className="font-semibold ">{product.title}</p>
-                  ))}
+                  {order.orderId}
                 </Table.Cell>
                 <Table.Cell>
                   {order.productsId.map(product => (
-                    <p key={product._id} className="font-semibold">{product.quantity}</p>
+                    <span key={product._id} className="font-semibold">{product.title}</span>
                   ))}
                 </Table.Cell>
+                
+                <Table.Cell>
+                  {order.productsId.map(product => (
+                    <span key={product._id} className="font-semibold">{product.quantity}</span>
+                  ))}
+                </Table.Cell>
+                
                 <Table.Cell>{order.subtotal}</Table.Cell>
                 <Table.Cell>{order.deliveryfee}</Table.Cell>
-                <Table.Cell >{order.totalcost}</Table.Cell>
-             </Table.Row>
+                <Table.Cell>{order.totalcost}</Table.Cell>
+            
+                
+                  
+              </Table.Row>
             </Table.Body>
-            ))}
-          
+          ))}
         </Table>
-        </>
         
-      ) : (
-        <p>You have no orders to show</p>
-      )}
-    </div>
-  );
-}
+      </>
+    ) : (
+      <p>You have no users yet!</p>
+    )}
+  </div>
+)}
