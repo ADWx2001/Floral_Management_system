1fe86f80bc2be587c1611415f4b14c230c449139
Rivin Jayawardhana <it22061652@my.sliit.lk>
2024-04-20 13:12:11 +0530

    Email send and report generation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -36,6 +36,7 @@ import Updatesuppliers from "./pages/Updatesupplier"
 import ForgetPassword from "./pages/ForgetPassword"
 import ResetPassword from "./pages/ResetPassword"
 import Addrestockrec from "./pages/addStockrecords"
+import ContactSup from "./pages/ContactSupplier"
 import CheckoutSuccess from "./pages/CheckoutSuccess"
 
 export default function App() {
@@ -83,6 +84,7 @@ export default function App() {
             <Route path="/ordersummary" element= {<Ordersummary/>} />
             <Route path="/checkout" element={<Checkout/>}/>
             <Route path="/order-pay-success" element={<CheckoutSuccess/>}/>
+            <Route path="/contactsup/:id" element={<ContactSup/>}/>
           </Routes>
         <Footer/>
       </>
