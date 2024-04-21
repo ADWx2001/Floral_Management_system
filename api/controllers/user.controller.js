12e01035f29cf1381aeb0f7bd36963bd075ef968
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-04-21 14:12:13 +0530

    User management updated with username validation part

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -38,15 +38,7 @@ export const updateUser = async (req,res,next) => {
                 errorHandler(400, 'Username must be between 7 and 20 characters')
               );
             }
-            if (req.body.username.includes(' ')) {
-              return next(errorHandler(400, 'Username cannot contain spaces'));
-            }
-           
-            if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-              return next(
-                errorHandler(400, 'Username can only contain letters and numbers')
-              );
-            }
+         
           }
 
           if (req.body.mobile) {
